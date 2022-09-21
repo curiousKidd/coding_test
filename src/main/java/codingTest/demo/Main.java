9e855cf210bfa7a1fc90511e072bc712ce0a16eb
curiousKidd <skasrjhaan@gmail.com>
2022-09-21 18:53:45 +0900

    20220921 - baekjoon codingTest

---
+++ src/main/java/codingTest/demo/Main.java
@@ -0,0 +1,13 @@
+package codingTest.demo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Programmers programmers = new Programmers();
+
+        programmers.mbtiTest();
+
+    }
+
+}
--- src/main/java/codingTest/demo/Programmers.java
+++ src/main/java/codingTest/demo/Programmers.java
@@ -1,60 +1,50 @@
 package codingTest.demo;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
-public class codingTest01 {
-
-    public static void main(String[] args) {
+public class Programmers {
+    public void mbtiTest(){
 
         String[] survey = {"AN", "CF", "MJ", "RT", "NA"};
         int[] choices = {5, 3, 2, 7, 5};
 
-        codingTest01 codingTest01 = new codingTest01();
-
-        codingTest01.test02(survey, choices);
-
-    }
-
-    public void test02(String[] survey, int[] choices){
-
-
-        char[] arrayText = {'T','E', 'S', 'T'};
+        char[] arrayText = {'T', 'E', 'S', 'T'};
         int[] arrayValue = {0,0,0,0};
 
         int[] value = {3,2,1,0,-1,-2,-3};
 
         for(int i=0; i < choices.length; i++){
-            int choiceValue = value[choices[i]-1];
 
+            int choiceValue = value[choices[i]-1];
             char[] chars = survey[i].toCharArray();
 
             switch (chars[0]){
                 case 'R':
                     arrayValue[0] = arrayValue[0] + choiceValue;
                     break;
-
                 case 'C':
                     arrayValue[1] = arrayValue[1] + choiceValue;
                     break;
-
-                case 'M':
+                case 'J':
                     arrayValue[2] = arrayValue[2] + choiceValue;
                     break;
-
                 case 'A':
                     arrayValue[3] = arrayValue[3] + choiceValue;
                     break;
 
-
+                case 'T':
+                    arrayValue[0] = arrayValue[0] - choiceValue;
+                    break;
+                case 'F':
+                    arrayValue[1] = arrayValue[1] - choiceValue;
+                    break;
+                case 'M':
+                    arrayValue[2] = arrayValue[2] - choiceValue;
+                    break;
+                case 'N':
+                    arrayValue[3] = arrayValue[3] - choiceValue;
+                    break;
             }
 
         }
-
-        System.out.println("arrayValue = " + arrayValue[0]);
-        System.out.println("arrayValue = " + arrayValue[1]);
-        System.out.println("arrayValue = " + arrayValue[2]);
-        System.out.println("arrayValue = " + arrayValue[3]);
 
         arrayText[0] = arrayValue[0] >= 0 ?  'R' : 'T' ;
         arrayText[1] = arrayValue[1] >= 0 ?  'C' : 'F' ;
