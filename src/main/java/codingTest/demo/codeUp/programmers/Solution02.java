474151ed3683b2c795b4c2d23fcb255a051cb163
curiousKidd <skasrjhaan@gmail.com>
2022-12-13 23:00:38 +0900

    20201213(화)

--- src/main/java/codingTest/demo/codeUp/programmers/Solution02.java
+++ src/main/java/codingTest/demo/codeUp/programmers/Solution02.java
@@ -9,7 +9,7 @@ public class Solution02 {
         int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
         String hand = "right";
 
-        int[] leftHand = {3, 1};
+        int[] leftHand = {3, 0};
         int[] rightHand = {3, 2};
 
         int[][] call = {
@@ -20,35 +20,42 @@ public class Solution02 {
         };
 
         for (int number : numbers) {
+            System.out.println("number = " + number);
             if (number == 1 || number == 4 || number == 7) {
                 answer += "L";
                 leftHand = call[number];
-                System.out.println("leftHand = " + leftHand);
             } else if (number == 3 || number == 6 || number == 9) {
                 answer += "R";
                 rightHand = call[number];
-                System.out.println("rightHand = " + rightHand);
             } else {
                 int value = call[number][0] + call[number][1];
+                int leftFirstValue = leftHand[0] - call[number][0];
                 int leftValue = leftHand[0] + leftHand[1] - value;
+                int rightFirstValue = rightHand[0] - call[number][0];
                 int rightValue = rightHand[0] + rightHand[1] - value;
 
-                if (Math.abs(leftValue) == Math.abs(rightValue)) {
-                    if (hand == "left") {
-                        answer += "L";
-                        leftHand = call[number];
-                        System.out.println("leftHand = " + leftHand);
-                    } else {
-                        answer += "R";
-                        rightHand = call[number];
-                        System.out.println("rightHand = " + rightHand);
-                    }
-                } else if (Math.abs(leftValue) > Math.abs(rightValue)) {
+                if (Math.abs(leftValue) > Math.abs(rightValue)) {
                     answer += "L";
                     leftHand = call[number];
-                } else {
+                } else if (Math.abs(leftValue) < Math.abs(rightValue)) {
                     answer += "R";
                     rightHand = call[number];
+                } else {
+                    if (Math.abs(leftFirstValue) < Math.abs(rightFirstValue)) {
+                        answer += "L";
+                        leftHand = call[number];
+                    } else if (Math.abs(leftFirstValue) > Math.abs(rightFirstValue)) {
+                        answer += "R";
+                        rightHand = call[number];
+                    } else {
+                        if (hand == "left") {
+                            answer += "L";
+                            leftHand = call[number];
+                        } else {
+                            answer += "R";
+                            rightHand = call[number];
+                        }
+                    }
                 }
 
             }
