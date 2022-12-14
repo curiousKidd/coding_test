fc56a051e710efa23c36962ff4d329b5ae269aab
curiousKidd <skasrjhaan@gmail.com>
2022-12-15 01:00:41 +0900

    20221215(목) - 영단어 숫자 치환하기

--- src/main/java/codingTest/demo/Main.java
+++ src/main/java/codingTest/demo/Main.java
@@ -1,16 +1,23 @@
 package codingTest.demo;
 
-import codingTest.demo.codeUp.programmers.Solution02;
+import codingTest.demo.codeUp.programmers.Solution05;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        KakaoCoding kakaoCoding = new KakaoCoding();
-
-        Solution02 solution = new Solution02();
+        Solution05 solution = new Solution05();
 
         solution.solution();
+
+        //        String[] arr = {"z", "b", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        //
+        //        int index = Arrays.binarySearch(arr, "z");
+        //        // binarySearch를 사용하면된다.
+        //        // 매개 변수로는 1. 배열의 이름 , 2. 해당 배열의 찾고자 하는 데이터
+        //        // 순서로 코드를 작성해주면 된다.
+        //
+        //        System.out.println(index + " 에 해당하는 값 -> ");
 
     }
 
