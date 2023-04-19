7134543e61c0543895c210aab8bfbce042c1b185
curiousKidd <skasrjhaan@gmail.com>
2023-04-19 16:45:07 +0900

    20230419(수) - 알고리즘 코딩

--- src/main/java/codingTest/demo/codeUp/programmers/cote_20230416/cote_01.java
+++ src/main/java/codingTest/demo/codeUp/programmers/cote_20230416/cote_01.java
@@ -18,13 +18,19 @@ public class cote_01 {
     public int solution(int[] numbers, int target) {
         int answer = 0;
         int n = numbers.length;
+
+        // 2차 배열을 for문에서 돌림으로 0 ~ N까지의 모든 경우의 수를 사용 할 수 있게 된다.
+
         for (int i = 0; i < (1 << n); i++) { // 2진수로 나타낼 수 있는 모든 경우의 수
+            System.out.println("i = " + i);
             int total = 0;
             for (int j = 0; j < n; j++) {
                 if ((i & (1 << j)) > 0) {
                     total += numbers[j]; // 더하기
+                    System.out.println("j1 = " + j);
                 } else {
                     total -= numbers[j]; // 빼기
+                    System.out.println("j2 = " + -j);
                 }
             }
             if (total == target) {
