0300921c4dc609841630b7723444f9e9405bc898
curiousKidd <skasrjhaan@gmail.com>
2023-04-30 19:05:58 +0900

    20230430(일) - 프로그래머스 스택 알고리즘

---
+++ src/main/java/codingTest/demo/codeUp/programmers/cote_20230430/cote01.java
@@ -0,0 +1,33 @@
+package codingTest.demo.codeUp.programmers.cote_20230430;
+
+import java.util.Stack;
+
+public class cote01 {
+
+    // https://school.programmers.co.kr/learn/courses/30/lessons/12906
+    // 같은 숫자는 싫어
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 3, 3, 0, 1, 1}
+
+        System.out.println("solution(A,B) = " + solution(arr));
+
+    }
+
+    public static int[] solution(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        for (int num : arr) {
+            if (stack.isEmpty() || stack.peek() != num) {
+                stack.push(num);
+            }
+        }
+
+        int[] answer = new int[stack.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = stack.get(i);
+        }
+
+        return answer;
+    }
+
+}
