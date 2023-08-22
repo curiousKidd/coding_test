ae1980d8fae5dc194b2509e9cc43317843af2c41
curiouskidd <skasrjhaan@gmail.com>
2023-08-22 14:43:24 +0900

    20230822(화) - 인프런 코딩테스트 강좌 수강

--- src/main/java/codingTest/demo/codeUp/inflearn/section01_String/caseConversion.java
+++ src/main/java/codingTest/demo/codeUp/inflearn/section01_String/caseConversion.java
@@ -4,7 +4,17 @@ import java.util.Scanner;
 
 public class caseConversion {
 
-    // StuDY
+    /**
+     * 대소문자 변환
+     * 대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로
+     * 변환하여 출력하는 프로그램을 작성하세요.
+     * <p>
+     * IN
+     * StuDY
+     * <p>
+     * OUT
+     * sTUdy
+     */
 
     public static void main(String[] args) {
         caseConversion T = new caseConversion();
--- src/main/java/codingTest/demo/codeUp/inflearn/section01_String/wordInSentence.java
+++ src/main/java/codingTest/demo/codeUp/inflearn/section01_String/wordInSentence.java
@@ -4,6 +4,18 @@ import java.util.Scanner;
 
 public class wordInSentence {
 
+    /**
+     * 문장 속 단어
+     * 대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로
+     * 변환하여 출력하는 프로그램을 작성하세요.
+     * <p>
+     * IN
+     * it is time to study
+     * <p>
+     * OUT
+     * study
+     */
+
     public static void main(String[] args) {
         wordInSentence T = new wordInSentence();
         Scanner kb = new Scanner(System.in);
