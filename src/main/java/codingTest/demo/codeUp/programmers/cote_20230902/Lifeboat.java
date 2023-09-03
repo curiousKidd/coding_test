31c4b0cdab45631d6c7fe193aba81c8df9dd4a6c
curiouskidd <skasrjhaan@gmail.com>
2023-09-03 19:56:01 +0900

    20230903(ì일) 프로그래머스 코테 구명보트

--- src/main/java/codingTest/demo/codeUp/programmers/cote_20230902/Lifeboat.java
+++ src/main/java/codingTest/demo/codeUp/programmers/cote_20230902/Lifeboat.java
@@ -1,5 +1,9 @@
 package codingTest.demo.codeUp.programmers.cote_20230902;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Lifeboat {
 
     public static void main(String[] args) {
@@ -13,18 +17,19 @@ public class Lifeboat {
 
     public static int solution(int[] people, int limit) {
         int answer = 0;
+        int left = 0;
+        int right = people.length - 1;
 
-        for (int i = 0; i < people.length; i++) {
-            int sum = 0;
-            if(people[i] != 0) {
-                for (int j = 0; j < people.length; j++) {
-                    if (sum + people[j] <= 100) {
-                        sum += people[j];
-                        people[j] = 0;
-                    }
-                }
-                answer++;
+        Arrays.sort(people);
+
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+                right--;
+            } else {
+                right--;
             }
+            answer++;
         }
 
         return answer;
