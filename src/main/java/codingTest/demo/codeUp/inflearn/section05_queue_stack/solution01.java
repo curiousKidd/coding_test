d8a683102ed968e11037f181c5ccf92848eacecd
curiousKidd <skasrjhaan@gmail.com>
2023-09-15 23:03:35 +0900

    20230915(금) - 인프런 강좌 stack queue

---
+++ src/main/java/codingTest/demo/codeUp/inflearn/section05_queue_stack/solution01.java
@@ -0,0 +1,29 @@
+package codingTest.demo.codeUp.inflearn.section05_queue_stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class solution01 {
+
+    public String solution(String str) {
+        String answer = "YES";
+        Stack<Character> stack = new Stack<>();
+        for (char x : str.toCharArray()) {
+            if (x == '(') stack.push(x);
+            else {
+                if (stack.isEmpty()) return "NO";
+                stack.pop();
+            }
+        }
+        if (!stack.isEmpty()) return "NO";
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+        solution01 T = new solution01();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+    }
+
+}
