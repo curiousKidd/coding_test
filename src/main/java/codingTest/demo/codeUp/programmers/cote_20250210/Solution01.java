458c492f437a9905148d341bbb7b242c88bfde35
curiousKidd <skasrjhaan@gmail.com>
2025-02-11 21:29:43 +0900

    test

--- src/main/java/codingTest/demo/codeUp/programmers/cote_20250210/Solution01.java
+++ src/main/java/codingTest/demo/codeUp/programmers/cote_20250210/Solution01.java
@@ -1,39 +1,38 @@
 package codingTest.demo.codeUp.programmers.cote_20250210;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class Solution01 {
 
-//    sizes	                                            result
-//    [[60, 50], [30, 70], [60, 30], [80, 40]]	        4000
-//    [[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]	    120
-//    [[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]	    133
+    //    sizes	                                            result
+    //    [[60, 50], [30, 70], [60, 30], [80, 40]]	        4000
+    //    [[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]	    120
+    //    [[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]	    133
 
     public static void main(String[] args) {
-        int[][] sizes = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+        //        int[][] sizes = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+        //        int[][] sizes = {{2, 6}, {1, 11}, {3, 7}};
+        //        int[][] sizes = {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}};
+        int[][] sizes = {{14, 4}, {19, 6}, {6, 16}, {18, 7}, {7, 11}};
         Solution01 solution01 = new Solution01();
         int solution = solution01.solution(sizes);
         System.out.println("solution = " + solution);
     }
 
     public int solution(int[][] sizes) {
-        int a = 0;
         int w = 0;
         int h = 0;
-        for(int[] size: sizes){
-            System.out.println("size = " + Arrays.toString(size));
-            int max = size[0] * size[1];
-
-            if(a < max ){
-                a = max;
+        for (int[] size : sizes) {
+            if (size[0] < size[1]) {
                 w = Math.max(w, size[0]);
                 h = Math.max(h, size[1]);
-                System.out.println("w = " + w);
-                System.out.println("h = " + h);
+            } else {
+                w = Math.max(w, size[1]);
+                h = Math.max(h, size[0]);
             }
         }
 
-        return w*h;
+        System.out.println("w = " + w);
+        System.out.println("h = " + h);
+
+        return w * h;
     }
 }
