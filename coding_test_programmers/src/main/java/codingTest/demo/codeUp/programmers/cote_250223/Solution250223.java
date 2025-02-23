8723aec2916c56fbd6d9259a04acd6cba8c3b3f5
curiousKidd <skasrjhaan@gmail.com>
2025-02-23 23:14:47 +0900

    update

---
+++ coding_test_programmers/src/main/java/codingTest/demo/codeUp/programmers/cote_250223/Solution250223.java
@@ -0,0 +1,15 @@
+package codingTest.demo.codeUp.programmers.cote_250223;
+
+public class Solution250223 {
+
+    public static void main(String[] args) {
+        System.out.println(solution("17"));
+        System.out.println(solution("011"));
+    }
+
+    public static int solution(String numbers) {
+
+        return 0;
+    }
+
+}
