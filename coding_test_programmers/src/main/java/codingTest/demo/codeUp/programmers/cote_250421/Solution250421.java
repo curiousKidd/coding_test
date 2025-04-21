00c175cc4b0c5fc68a2fb22b1eb2ed53e099a1be
curiousKidd <skasrjhaan@gmail.com>
2025-04-21 23:49:41 +0900

    programmers coding test hashMap

---
+++ coding_test_programmers/src/main/java/codingTest/demo/codeUp/programmers/cote_250421/Solution250421.java
@@ -0,0 +1,68 @@
+package codingTest.demo.codeUp.programmers.cote_250421;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution250421 {
+
+    //https://school.programmers.co.kr/learn/courses/30/lessons/42576
+    //완주하지 못한 선수
+
+    public static void main(String[] args) {
+        String[] participant1 = {"leo", "kiki", "eden"};
+        String[] completion1 = {"eden", "kiki"};
+        // "leo"
+
+        String[] participant2 = {"marina", "josipa", "nikola", "vinko", "filipa"};
+        String[] completion2 = {"josipa", "filipa", "marina", "nikola"};
+        // "vinko"
+
+        String[] participant3 = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion3 = {"stanko", "ana", "mislav"};
+        // "mislav"
+
+        //        System.out.println(solution(participant1, completion1));
+        //        System.out.println(solution(participant2, completion2));
+        System.out.println(solution(participant3, completion3));
+    }
+
+    //    public static String solution(String[] participant, String[] completion) {
+    //        StringBuilder answer = new StringBuilder();
+    //
+    //        int counter = 0;
+    //
+    //        for (String name : participant) {
+    //            for (int i = 0; i < completion.length; i++) {
+    //                if (name.equals(completion[i])) {
+    //                    completion[i] = Strings.EMPTY;
+    //                    counter = 0;
+    //                    break;
+    //                }
+    //
+    //                counter++;
+    //            }
+    //            if (counter == completion.length) answer.append(name);
+    //        }
+    //
+    //        return answer.toString();
+    //    }
+
+    public static String solution(String[] participant, String[] completion) {
+        Map<String, Integer> countMap = new HashMap<>();
+        for (String p : participant) {
+            countMap.put(p, countMap.getOrDefault(p, 0) + 1);
+        }
+
+        for (String c : completion) {
+            countMap.put(c, countMap.get(c) - 1);
+        }
+        
+        for (Map.Entry<String, Integer> entry : countMap.entrySet()) {
+            if (entry.getValue() > 0) {
+                return entry.getKey();
+            }
+        }
+        return ""; // 문제 조건상 절대 도달하지 않음
+    }
+
+}
