154384ec88e97828f203e078805c98ea9a59f261
curiousKidd <skasrjhaan@gmail.com>
2025-04-26 23:37:51 +0900

    hash 폰켓몬 init

---
+++ coding_test_programmers/src/main/java/codingTest/demo/codeUp/programmers/cote_250426/Solution250426.java
@@ -0,0 +1,23 @@
+package codingTest.demo.codeUp.programmers.cote_250426;
+
+public class Solution250426 {
+
+    //https://school.programmers.co.kr/learn/courses/30/lessons/1845
+    //폰켓몬
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {3, 1, 2, 3};
+        int[] nums2 = {3, 3, 3, 2, 2, 4};
+        int[] nums3 = {3, 3, 3, 2, 2, 2};
+
+        System.out.println(solution(nums1));
+    }
+
+    public static int solution(int[] nums) {
+        int answer = 0;
+        
+        return answer;
+    }
+
+}
