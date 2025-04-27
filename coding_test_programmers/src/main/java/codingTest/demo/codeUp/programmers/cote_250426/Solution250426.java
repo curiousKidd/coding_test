5ab9936f59c7ba7d6947518e9f42f3ed9f7ca0d3
curiousKidd <skasrjhaan@gmail.com>
2025-04-27 18:21:20 +0900

    폰켓몬 test code add

--- coding_test_programmers/src/main/java/codingTest/demo/codeUp/programmers/cote_250426/Solution250426.java
+++ coding_test_programmers/src/main/java/codingTest/demo/codeUp/programmers/cote_250426/Solution250426.java
@@ -1,5 +1,8 @@
 package codingTest.demo.codeUp.programmers.cote_250426;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution250426 {
 
     //https://school.programmers.co.kr/learn/courses/30/lessons/1845
@@ -12,11 +15,24 @@ public class Solution250426 {
         int[] nums3 = {3, 3, 3, 2, 2, 2};
 
         System.out.println(solution(nums1));
+        System.out.println(solution(nums2));
+        System.out.println(solution(nums3));
     }
 
     public static int solution(int[] nums) {
         int answer = 0;
-        
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        if (map.size() < nums.length / 2) {
+            answer = map.size();
+        } else {
+            answer = nums.length / 2;
+        }
+
         return answer;
     }
 
---
+++ coding_test_programmers/src/test/java/codingTest/demo/cote_250426/Solution250426Test.java
@@ -0,0 +1,53 @@
+package codingTest.demo.cote_250426;
+
+import codingTest.demo.codeUp.programmers.cote_250426.Solution250426;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Solution250426Test {
+
+    @Test
+    @DisplayName("nums1 = {3, 1, 2, 3} → 기대값=2")
+    void testNums1() {
+        int[] nums1 = {3, 1, 2, 3};
+        assertEquals(2, Solution250426.solution(nums1));
+    }
+
+    @Test
+    @DisplayName("nums2 = {3, 3, 3, 2, 2, 4} → 기대값=3")
+    void testNums2() {
+        int[] nums2 = {3, 3, 3, 2, 2, 4};
+        assertEquals(3, Solution250426.solution(nums2));
+    }
+
+    @Test
+    @DisplayName("nums3 = {3, 3, 3, 2, 2, 2} → 기대값=2")
+    void testNums3() {
+        int[] nums3 = {3, 3, 3, 2, 2, 2};
+        assertEquals(2, Solution250426.solution(nums3));
+    }
+
+    @Test
+    @DisplayName("빈 배열 → 기대값=0")
+    void testEmpty() {
+        int[] empty = {};
+        assertEquals(0, Solution250426.solution(empty));
+    }
+
+    @Test
+    @DisplayName("모두 동일한 값, 짝수 개수 → 기대값=1")
+    void testAllSameEven() {
+        int[] nums = {5, 5, 5, 5};
+        assertEquals(1, Solution250426.solution(nums));
+    }
+
+    @Test
+    @DisplayName("모두 동일한 값, 홀수 개수 → 기대값=1")
+    void testAllSameOdd() {
+        int[] nums = {7, 7, 7, 7, 7};
+        assertEquals(1, Solution250426.solution(nums));
+    }
+
+}
