a12337b6ccfe5210ea4c46e53a264ab4c69f0382
curiousKidd <skasrjhaan@gmail.com>
2025-06-01 23:41:56 +0900

    프로그래머스 탐욕법 > 체육복 완료

--- coding_test_programmers/src/main/java/codingTest/demo/codeUp/programmers/cote_250526/Solution250526.java
+++ coding_test_programmers/src/main/java/codingTest/demo/codeUp/programmers/cote_250526/Solution250526.java
@@ -1,7 +1,6 @@
 package codingTest.demo.codeUp.programmers.cote_250526;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Solution250526 {
 
@@ -18,30 +17,47 @@ public class Solution250526 {
     }
 
     public static int solution(int n, int[] lost, int[] reserve) {
-        int answer = n;
-        Map<Integer, Integer> map = new HashMap<>();
-
-        //잃어버린
-        for (int i = 0; i < lost.length; i++) {
-            int lostNumber = lost[i];
-            map.put(lostNumber, map.getOrDefault(lostNumber, 0) - 1);
+        // 1. Set으로 변환하여 '도난만 당한 학생'과 '여벌만 있는 학생' 분리
+        Set<Integer> lostSet = new HashSet<>();
+        for (int l : lost) {
+            lostSet.add(l);
+        }
+        Set<Integer> reserveSet = new HashSet<>();
+        for (int r : reserve) {
+            reserveSet.add(r);
         }
 
-        //여유
-        for (int i = 0; i < reserve.length; i++) {
-            int reserveNumber = reserve[i];
-            map.put(reserveNumber, map.getOrDefault(reserveNumber, 0) + 1);
+        // 도난과 여벌이 겹치는 학생(=lost∩reserve)을 제거
+        Set<Integer> intersection = new HashSet<>(lostSet);
+        intersection.retainAll(reserveSet);
+        for (int x : intersection) {
+            lostSet.remove(x);
+            reserveSet.remove(x);
         }
 
-        //카운트
-        // 이러면 바른 계산이 안되지
-        for (Map.Entry<Integer, Integer> cnt : map.entrySet()) {
-            if (cnt.getValue() < 0) {
-                answer += cnt.getValue();
+        // 2. 남은 lostSet, reserveSet을 정렬된 리스트로 변환
+        List<Integer> lostList = new ArrayList<>(lostSet);
+        List<Integer> reserveList = new ArrayList<>(reserveSet);
+        Collections.sort(lostList);
+        Collections.sort(reserveList);
+
+        // 3. 그리디하게 인접 번호 학생에게 체육복 대여 시도
+        int unableCount = 0;
+        for (int l : lostList) {
+            if (reserveList.contains(l - 1)) {
+                // 왼쪽 이웃에게 빌림
+                reserveList.remove(Integer.valueOf(l - 1));
+            } else if (reserveList.contains(l + 1)) {
+                // 오른쪽 이웃에게 빌림
+                reserveList.remove(Integer.valueOf(l + 1));
+            } else {
+                // 빌릴 수 없으면 count 증가
+                unableCount++;
             }
-
         }
 
-        return answer;
+        // 4. 체육수업에 참여 가능한 학생 수 = 전체 학생 수 - 빌리지 못한 학생 수
+        return n - unableCount;
     }
 }
+
