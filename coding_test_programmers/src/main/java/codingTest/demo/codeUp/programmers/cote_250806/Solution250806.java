526b9f733a400fe04dc78d32ee38e05965e3e11d
curiouskidd <skasrjhaan@gmail.com>
2025-08-07 23:39:37 +0900

    프로그래머스 지케차와 크레인

---
+++ coding_test_programmers/src/main/java/codingTest/demo/codeUp/programmers/cote_250806/Solution250806.java
@@ -0,0 +1,20 @@
+package codingTest.demo.codeUp.programmers.cote_250806;
+
+public class Solution250806 {
+
+    // https://school.programmers.co.kr/learn/courses/30/lessons/388353
+    // 지게차와 크레인
+
+    public static void main(String[] args) {
+        String[] storage1 = {"AZWQY", "CAABX", "BBDDA", "ACACA"};
+        String[] requests1 = {"A", "BB", "A"};
+
+        System.out.println("solution(storage1, requests1) = " + solution(storage1, requests1)); // 11
+    }
+
+    public static int solution(String[] storage, String[] requests) {
+        int answer = 0;
+        return answer;
+    }
+
+}
