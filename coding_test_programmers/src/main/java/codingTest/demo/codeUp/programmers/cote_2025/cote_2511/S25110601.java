1c72558ed28e23022e8a3e47b42c849cf0f24711
curiouskidd <skasrjhaan@gmail.com>
2025-11-11 22:18:03 +0900

    프로그래머스 251111

--- coding_test_programmers/src/main/java/codingTest/demo/codeUp/programmers/cote_2025/cote_2511/S25110601.java
+++ coding_test_programmers/src/main/java/codingTest/demo/codeUp/programmers/cote_2025/cote_2511/S25110601.java
@@ -1,14 +1,11 @@
 package codingTest.demo.codeUp.programmers.cote_2025.cote_2511;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class S25110601 {
 
     // https://school.programmers.co.kr/learn/courses/30/lessons/159994
 
     public static void main(String[] args) {
-        String[] cards1 = {"i", "water", "drink"};
+        String[] cards1 = {"i", "drink", "water"};
         String[] cards2 = {"want", "to"};
         String[] goal = {"i", "want", "to", "drink", "water"};
 
@@ -25,19 +22,29 @@ public class S25110601 {
     public static String solution(String[] cards1, String[] cards2, String[] goal) {
         String answer = "Yes";
 
-        HashMap<String, Integer> map = new HashMap<>();
-        HashMap<String, Integer> goalMap = new HashMap<>();
+        int card1Index = 0;
+        int card2Index = 0;
 
-        for (int i = 0; i <= 10; i++) {
-            if (i < cards1.length) map.put(cards1[i], map.getOrDefault(cards1[i], 0) + 1);
-            if (i < cards2.length) map.put(cards2[i], map.getOrDefault(cards2[i], 0) + 1);
-            if (i < goal.length) goalMap.put(goal[i], map.getOrDefault(goal[i], 0) + 1);
+        for (String g : goal) {
+            String card1 = "";
+            String card2 = "";
+
+            if (card1Index < cards1.length) {
+                card1 = cards1[card1Index];
+            }
+            if (card2Index < cards2.length) {
+                card2 = cards2[card2Index];
+            }
+
+
+            System.out.println("g = " + g);
+            System.out.println("card1 = " + card1);
+            System.out.println("card2 = " + card2);
+
+            if (g.equals(card1)) card1Index++;
+            else if (g.equals(card2)) card2Index++;
+            else return "No";
         }
-
-        for (Map.Entry<String, Integer> key : goalMap.entrySet()) {
-            if (map.getOrDefault(key.getKey(), 0) == 0) return "No";
-        }
-
 
         return answer;
     }
